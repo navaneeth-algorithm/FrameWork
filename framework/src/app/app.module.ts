47e231601973a8c38c28dcdcc97d9d69fe9cd688
navaneeth <navaneethb807@gmail.com>
2019-08-22 05:34:36 +0530

    Assg1UDemy

--- framework/src/app/app.module.ts
+++ framework/src/app/app.module.ts
@@ -1,11 +1,19 @@
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { ServerComponent } from './server/server.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule
---
+++ framework/src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'app-warningalert',
+    templateUrl:'./warning-alert.component.html',
+    styleUrls:['./warning-alert.component.css']
+})
+export class WarningAlertComponent{
+
+}
